fd5f2a61b6c595693853d6ecb78cd85f96486d2e
Peeyush Sengar <peeyushsengar415@gmail.com>
2023-04-17 02:16:01 +0530

    changes in rtl arabic header

--- packages/Webkul/Velocity/src/Resources/views/shop/products/list/toolbar.blade.php
+++ packages/Webkul/Velocity/src/Resources/views/shop/products/list/toolbar.blade.php
@@ -88,14 +88,14 @@
                 @endif
             </div>
 
-            <div class="col-2" @click="toggleLayeredNavigation({event: $event, actionType: 'open'})">
+            <div class="col-2 col-filter" @click="toggleLayeredNavigation({event: $event, actionType: 'open'})">
                 <a class="unset">
                     <i class="material-icons">filter_list</i>
                     <span>{{ __('velocity::app.shop.general.filter') }}</span>
                 </a>
             </div>
 
-            <div class="col-5">
+            <div class="col-5 col-order">
                 <div class="sorter" id="sort-by">
 
                     <select class="selective-div border-normal styled-select" onchange="window.location.href = this.value">
@@ -112,7 +112,7 @@
                 </div>
             </div>
 
-            <div class="col-3">
+            <div class="col-3 col-count">
                 <div class="limiter" id="limit-by">
 
                     <select class="selective-div border-normal styled-select" onchange="window.location.href = this.value" style="width: 57px;" aria-label="Show">
@@ -133,7 +133,7 @@
                 </div>
             </div>
 
-            <div class="col-2">
+            <div class="col-2 col-view">
                 @php
                     $isList = $toolbarHelper->isModeActive('list');
                 @endphp
@@ -159,25 +159,23 @@
         (() => {
             Vue.component('toolbar-component', {
                 template: '#toolbar-template',
-                data: function () {
+                data() {
                     return {
-                        'layeredNavigation': false,
+                        layeredNavigation: false,
                         currentScreen : window.innerWidth,
                     }
                 },
 
-                created: function () {
+                created() {
                     window.addEventListener('resize', this.handleResize);
-
-                    this.handleResize();
                 },
 
-                destroyed: function () {
+                destroyed() {
                     window.removeEventListener('resize', this.handleResize);
                 },
 
                 watch: {
-                    layeredNavigation: function (value) {
+                    layeredNavigation(value) {
                         if (value) {
                             document.body.classList.add('open-hamburger');
                         } else {
@@ -187,11 +185,11 @@
                 },
 
                 methods: {
-                    toggleLayeredNavigation: function ({event, actionType}) {
+                    toggleLayeredNavigation({event, actionType}) {
                         this.layeredNavigation = !this.layeredNavigation;
                     },
 
-                    handleResize: function () {
+                    handleResize() {
                         this.currentScreen = window.innerWidth;
                     }
                 }
